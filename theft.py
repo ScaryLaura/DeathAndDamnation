470c29beb771fe1d0e581f8f89723f120c5f2427
ScaryLaura <103382980+ScaryLaura@users.noreply.github.com>
2022-04-10 16:26:54 -0300

    Create theft.py

---
+++ theft.py
@@ -0,0 +1,22 @@
+def get_all_values(d):
+    if isinstance(d, dict):
+        for v in d.values():
+            yield from get_all_values(v)
+    elif isinstance(d, list):
+        for v in d:
+            yield from get_all_values(v)
+    else:
+        yield d
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
